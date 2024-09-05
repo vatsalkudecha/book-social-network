1c4bd2c391568c6014b4a2a020d4ab1b2a47f5a6
vatsalkudecha <1311vatsal@gmail.com>
2024-09-05 14:34:14 +0530

    Implement the menu component

---
+++ book-network-ui/src/app/module/book/book.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BookRoutingModule } from './book-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BookListComponent } from './pages/book-list/book-list.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    MenuComponent,
+    BookListComponent
+  ],
+  imports: [
+    CommonModule,
+    BookRoutingModule
+  ]
+})
+export class BookModule { }
