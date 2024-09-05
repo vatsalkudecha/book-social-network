58a814c6fe664567e522b1930c4ba2e7610f8e11
vatsalkudecha <1311vatsal@gmail.com>
2024-09-05 14:56:41 +0530

    Implement book-list page in progress

--- book-network-ui/src/app/module/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/module/book/pages/book-list/book-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../../../services/services/book.service';
+import { Router } from '@angular/router';
+import { findAllBooks } from '../../../../services/fn/book/find-all-books';
+import { PageResponseBookResponse } from '../../../../services/models/page-response-book-response';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  bookResponse: PageResponseBookResponse = {};
+  page: number = 0;
+  size: number = 5;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books: PageResponseBookResponse): void => {
+        this.bookResponse = books;
+      }
+    })
+  }
 
 }
