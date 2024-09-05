ba12bc83b13bc10aa74b6cfa63258151b7125f4e
vatsalkudecha <1311vatsal@gmail.com>
2024-09-05 20:24:32 +0530

    Minor change

--- book-network/src/main/java/com/project/book/book/BookRepository.java
+++ book-network/src/main/java/com/project/book/book/BookRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
 
