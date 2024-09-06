77d16b293ad3d88a8f20dab0a2f44bc416b37c6d
vatsalkudecha <1311vatsal@gmail.com>
2024-09-06 18:52:05 +0530

    Implement my-book component

---
+++ book-network-ui/src/app/module/book/pages/my-books/my-books.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../../../../services/services/book.service';
+import { Router } from '@angular/router';
+import { findAllBooks } from '../../../../services/fn/book/find-all-books';
+import { PageResponseBookResponse } from '../../../../services/models/page-response-book-response';
+import { BookResponse } from '../../../../services/models/book-response';
+
+
+@Component({
+  selector: 'app-my-books',
+  templateUrl: './my-books.component.html',
+  styleUrls: ['./my-books.component.scss']
+})
+export class MyBooksComponent implements OnInit {
+
+  bookResponse: PageResponseBookResponse = {};
+  page: number = 0;
+  size: number = 4;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  findAllBooks() {
+    this.bookService.findAllBooksByOwner({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books: PageResponseBookResponse): void => {
+        this.bookResponse = books;
+      }
+    })
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToPage(pageIndex: number) {
+    this.page = pageIndex;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  get isLastPage(): boolean {
+    return this.page == this.bookResponse.totalPages as number - 1;
+  }
+
+  archiveBook(book: BookResponse) {
+
+  }
+
+  shareBook(book: BookResponse) {
+    
+  }
+
+  editBook(book: BookResponse) {
+
+  }
+}
