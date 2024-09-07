449ce19d50c06795725f1f1800809f2e5dbebba2
vatsalkudecha <1311vatsal@gmail.com>
2024-09-08 00:33:26 +0530

    Securing routes with authGuard

--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,25 @@ export class TokenService {
 
   get token() {
     return localStorage.getItem('token') as string;
+  }
+
+  isTokenNotValid() {
+    return !this.isTokenValid();
+  }
+
+  private isTokenValid() {
+    const token = this.token;
+    if (!token) {
+      return false;
+    }
+    // decode the token
+    const jwtHelper = new JwtHelperService();
+    // check the expiry date
+    const isTokenExpired = jwtHelper.isTokenExpired(token);
+    if(isTokenExpired) {
+      localStorage.clear();
+      return false;
+    }
+    return true;
   }
 }
