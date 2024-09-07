1c70850e8beeb30fa5d7c0b1ea0371be7cf91853
vatsalkudecha <1311vatsal@gmail.com>
2024-09-07 19:16:14 +0530

    Implement update-book feature

--- book-network-ui/src/app/module/book/pages/manage-book/manage-book.component.ts
+++ book-network-ui/src/app/module/book/pages/manage-book/manage-book.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookRequest } from '../../../../services/models/book-request';
 import { BookService } from '../../../../services/services/book.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookResponse } from '../../../../services/models/book-response';
 
 @Component({
   selector: 'app-manage-book',
   templateUrl: './manage-book.component.html',
   styleUrl: './manage-book.component.scss'
 })
-export class ManageBookComponent {
+export class ManageBookComponent implements OnInit {
   
   bookRequest: BookRequest = {authorName: '', isbn:'', synopsis:'', title:''};
   errorMsg: Array<string> = [];
@@ -17,8 +18,32 @@ export class ManageBookComponent {
 
   constructor(
     private bookService: BookService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ){}
+
+  ngOnInit(): void {
+    const bookId = this.activatedRoute.snapshot.params['bookId'];
+    if (bookId) {
+      this.bookService.findByBookId({
+        'book-id': bookId
+      }).subscribe({
+        next: (book: BookResponse) => {
+          this.bookRequest = {
+            id: book.id,
+            title: book.title as string,
+            authorName: book.authorName as string,
+            isbn: book.isbn as string,
+            synopsis: book.synopsis as string,
+            shareable: book.shareable
+          }
+          if(book.cover) {
+            this.selectedPicture = 'data:image/jpg;base64,' + book.cover;
+          }
+        }
+      })
+    }
+  }
 
   onFileSelected(event: any) {
     this.selectedBookCover = event.target.files[0];
--- book-network-ui/src/app/module/book/pages/my-books/my-books.component.ts
+++ book-network-ui/src/app/module/book/pages/my-books/my-books.component.ts
@@ -77,6 +77,6 @@ export class MyBooksComponent implements OnInit {
   }
 
   editBook(book: BookResponse) {
-
+    this.router.navigate(['books', 'manage', book.id])
   }
 }
